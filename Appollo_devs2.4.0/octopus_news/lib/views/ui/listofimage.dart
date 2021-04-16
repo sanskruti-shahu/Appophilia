3caa1358095166369d640d2b9f4d152c76392160
sanskruti_shahu <lcs2020056@iiitl.ac.in>
2021-04-16 16:18:29 +0530

    commit 3

---
+++ Appollo_devs2.4.0/octopus_news/lib/views/ui/listofimage.dart
@@ -0,0 +1,5 @@
+const List<String> LatestnewsImages = [
+  "lib/assets/images/deep.jpg",
+  "lib/assets/images/rahul.jpg",
+  "lib/assets/images/taylor.jpg",
+];
--- Appollo_devs2.4.0/octopus_news/lib/views/ui/main_page.dart
+++ Appollo_devs2.4.0/octopus_news/lib/views/ui/main_page.dart
@@ -1,4 +1,25 @@
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:octopus_news/views/ui/listofimage.dart';
+import 'package:sliver_tools/sliver_tools.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+    );
+  }
+}
 
 class MainPage extends StatefulWidget {
   @override
@@ -31,31 +52,127 @@ class _MainPageState extends State<MainPage> with SingleTickerProviderStateMixin
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text('Hello,'),
-                Text(" "),
-                TabBar(tabs: [
-                  Tab(
-                    child: Text('Latest News'),
-                  ),
-                  Tab(
-                    child: Text('Activities'),
-                  ),
-                  Tab(
-                    child: Text('Activities'),
-                  ),
-                ],
-                  controller: _tabController,
-                )
-              ],
+          body: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (context, value) {
+          return [
+            SliverAppBar(
+              floating: false,
+              pinned: false,
+              expandedHeight: 50,
             ),
+            SliverAppBar(
+              leading: Container(),
+              expandedHeight: 250.0,
+              floating: false,
+              pinned: true,
+            ),
+            SliverPinnedHeader(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: TabBar(
+                    indicator: BubbleTabIndicator(
+                      indicatorHeight: 45,
+                      indicatorRadius: 10,
+                      insets: EdgeInsets.only(left: 30, right: 30),
+                      indicatorColor: Colors.black12,
+                      tabBarIndicatorSize: TabBarIndicatorSize.tab,
+                    ),
+                    overlayColor:
+                    MaterialStateProperty.all(Colors.transparent),
+                    enableFeedback: true,
+                    controller: _tabController,
+                    tabs: [
+                      Tab(
+                        child:Text("Latest news")
+                        ),
+                      Tab(
+                        child: Text("Activities")
+                      ),
+                    ],
+                  ),
+                ),
+                color: Colors.white,
+              ),
+
+            )
+          ];
+        },
+        body: Container(
+          child: TabBarView(
+          controller: _tabController,
+            children: [
+              ListView.builder(
+                itemCount: LatestnewsImages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding:
+                    EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                    height: 240,
+                    width: double.infinity,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(LatestnewsImages[index]),
+                            fit: BoxFit.fill
+                        ),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              ListView.builder(
+                itemCount: LatestnewsImages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding:
+                    EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                    height: 240,
+                    width: double.infinity,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(LatestnewsImages[index]),
+                            fit: BoxFit.fill),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              ListView.builder(
+                itemCount: LatestnewsImages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding:
+                    EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                    height: 240,
+                    width: double.infinity,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(LatestnewsImages[index]),
+                            fit: BoxFit.fill),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        )
+        ),
+    )
+    ),
     );
   }
 }
+
